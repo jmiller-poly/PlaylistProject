6b61ffc57bbfb9087c342ef82f65482bf79161b5
James Miller <jamesmiller2024@gmail.com>
2023-01-11 14:55:53 -0800

    Finished the Song object.

--- Song.java
+++ Song.java
@@ -26,13 +26,68 @@ public class Song {
         liked = false;
     }
 
-     /**
-      * Methods-- what will you want each Song to do?
-      * Consider all the getter methods (getName, getArtist, etc.)
-      * You should probably have a toString method to be able to get the information for the full
-      * song easily as well!
-      * What kind of mutator (setter) methods will you need?
-      */
+     //methods//
+    
+    /*
+     * Returns the title of the song
+     */
+    public String getTitle() {
+        return title;
+    }
 
+    /*
+     * Returns the author of the song
+     */
+    public String getAuthor() {
+        return author;
+    }
 
+    /*
+     * returns the length in raw seconds
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /*
+     * Converts the length of the song from seconds to a standard minute:second format
+     */
+    public String getLengthMinutes() {
+        String sLen = "";
+        int min = length/60;
+        int sec = length%60;
+        sLen += min;
+        sLen += ":";
+        if(sec < 10) { //Necessary to format, otherwise lengths with seconds fewer than 10 could look something like 4:2 instead of 4:02
+            sLen += "0";
+        }
+        sLen += sec;
+        return sLen;
+    }
+
+    /*
+     * Returns if the song is liked
+     */
+    public boolean isLiked() {
+        return liked;
+    }
+
+    /*
+     * Returns a string with the following information about the song: Title, Author, Length, Liked
+     */
+    public String toString() {
+        if(liked == true) {
+            return "\"" + title + "\"" + " by " +  author + " (" + getLengthMinutes() + ") -- liked";
+        }
+        else {
+            return "\"" + title + "\"" + " by " +  author + " (" + getLengthMinutes() + ")";
+        }
+    }
+
+    /*
+     * Inverts the "liked" status, if the song was previously unliked, it is now liked. Otherwise, it is now unliked. 
+     */
+    public void like() {
+        liked = !liked; 
+    }
 }
