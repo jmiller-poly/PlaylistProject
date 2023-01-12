98701dfe739b3c12aa0e2be486a3c0f1587a1a13
James Miller <jmiller24@students.polytechnic.org>
2023-01-12 12:13:08 -0800

    Wrote all methods for Playlist object.

--- Playlist.java
+++ Playlist.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
  */
 public class Playlist {
     //fields//
+
     ArrayList<Song> songs;
 
     //constructor//
+    
     public Playlist() {
         ArrayList<Song> songs = new ArrayList<Song>();
     }
@@ -33,28 +35,80 @@ public class Playlist {
         if(pos-2 < songs.size()) {
             return true;
         } else {
-            System.out.println("Invalid index");
+            System.out.println("Invalid index.");
             return false;
         }
     }
 
-
+    /*
+     * Likes or unlikes a song
+     */
     public void likeSong(int position) {
         boolean valid = indexCheck(position);
         if(valid == true){
-            songs.get(position).like(position-1); //position as dictated by the printSongList method is always 1 more than the true index
+            songs.get(position).like(); //position as dictated by the printSongList method is always 1 more than the true index
         }
     }
 
+    /*
+     * Adds a new song to the playlist given the information for the song
+     */
+    public void addSongWithInfo(String title, String author, int length) {
+        Song song = new Song(title, author, length);
+        songs.add(song);
+    }
 
-     /**
-       * Methods-- Remember that you need to be able to complete all of the following:
-       * Adding a song
-       * 'liking' a song
-       * Removing a specific song
-       * Examining all Songs (a String return or void print makes sense here)
-       * Examining a sublist of all liked songs
-       * Determining the total duration of all songs
-       * Removing all unliked songs from the playlist (careful with this one!)
-       */
+    /*
+     * Adds a new song given the song as an object
+     */
+    public void addSong(Song s) {
+        songs.add(s);
+    }
+
+    /*
+     * Removes a song from the playlist
+     */
+    public void removeSong(int position) {
+        boolean valid = indexCheck(position);
+        if(valid == true) {
+            songs.remove(position);
+        } else {
+            System.out.println("Invalid index.");
+        }
+    }
+
+    public void printLikedSongs() {
+        for(Song s : songs) {
+            if(s.isLiked()) {
+                System.out.println(s);
+            }
+        }
+    }
+
+    public String totalDuration() {
+        int length = 0;
+        for(Song s : songs) {
+            length += s.getLength();
+        }
+
+        String sLen = "";
+        int min = length/60;
+        int sec = length%60;
+        sLen += min;
+        sLen += ":";
+        if(sec < 10) { //Necessary to format, otherwise lengths with seconds fewer than 10 could look something like 4:2 instead of 4:02
+            sLen += "0";
+        }
+        sLen += sec;
+        return sLen;
+    }
+
+    public void removeUnliked() {
+        for(int i = 0; i < songs.size(); i++) {
+            if(!(songs.get(i).isLiked())) {
+                songs.remove(i);
+                i--;
+            }
+        }
+    }
 }
